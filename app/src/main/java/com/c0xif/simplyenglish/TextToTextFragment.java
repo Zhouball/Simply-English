360cb8ed71a63db54acaf6abaccf475e76004ac9
Matthew Kuzdal <mkuzdal@gmail.com>
2017-01-14 13:32:17 -0800

    working

--- app/src/main/java/com/c0xif/simplyenglish/TextToTextFragment.java
+++ app/src/main/java/com/c0xif/simplyenglish/TextToTextFragment.java
@@ -6,8 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class TextToTextFragment extends Fragment {
+
+    private TextView text;
+    private Button submit;
 
     public static TextToTextFragment newInstance() {
         return new TextToTextFragment();
@@ -19,8 +25,20 @@ public class TextToTextFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_text_to_text, container, false);
+        View view = inflater.inflate(R.layout.fragment_text_to_text, container, false);
+        submit = (Button) view.findViewById(R.id.submit_button);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                    
+            }
+        });
+
+        text = (TextView) view.findViewById(R.id.edit_text);
+        return view;
     }
+
+
 
 }
 
