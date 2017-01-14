f4547237f67405495bd2adc1a817d336d2c4b0c6
Daniel Hosseinian <Daniel Hosseinian>
2017-01-14 04:28:01 -0800

    Added ability to change text in top fragment

--- app/src/main/java/com/c0xif/simplyenglish/MainActivity.java
+++ app/src/main/java/com/c0xif/simplyenglish/MainActivity.java
@@ -2,6 +2,7 @@ package com.c0xif.simplyenglish;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void buttonClick(View v)
+    {
+        TextView tv = (TextView)findViewById(R.id.textView);
+        tv.setText("This reflects a change");
     }
 }
--- app/src/main/java/com/c0xif/simplyenglish/SpeechToTextFragment.java
+++ app/src/main/java/com/c0xif/simplyenglish/SpeechToTextFragment.java
@@ -6,11 +6,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class SpeechToTextFragment extends Fragment {
 
-    public static TextToSimpleFragment newInstance() {
-        return new TextToSimpleFragment();
+    public static SpeechToTextFragment newInstance() {
+        return new SpeechToTextFragment();
     }
 
     public SpeechToTextFragment() {
@@ -21,5 +22,6 @@ public class SpeechToTextFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_speech_to_text, container, false);
     }
+
 }
 
